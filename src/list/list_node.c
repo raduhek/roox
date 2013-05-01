1451f0d39d387b52f5a27a2ac48fc9ca50e52a40
Cruceru Radu <radu.cruceru@gmail.com>
2013-05-01 15:22:55 +0300

    modified list_node structure and new_list_node to save a reference to a trie_node

--- src/list/list_node.c
+++ src/list/list_node.c
@@ -5,10 +5,11 @@
 /*
     Initialize a new node for the binary tree
 */
-list_node_t *new_list_node(char c) {
+list_node_t *new_list_node(char c, struct trie_node_struct *to) {
     list_node_t *t = (list_node_t*)malloc(sizeof(list_node_t));
     t->reach_char = c;
     t->next = NULL;
+    t->to = to;
 
     return t;
 }
--- src/list/list_node.h
+++ src/list/list_node.h
@@ -1,12 +1,15 @@
 #ifndef ROOX_LIST_NODE_H
 #define ROOX_LIST_NODE_H
 
+struct trie_node_struct;
+
 typedef struct list_node_struct {
     char reach_char;
     struct list_node_struct *next;
+    struct trie_node_struct *to;
 } list_node_t;
 
-list_node_t *new_list_node(char);
+list_node_t *new_list_node(char, struct trie_node_struct *);
 
 #endif
 
