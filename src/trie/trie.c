641c9859396f7348c9f11e4b01191eaba6bdcaff
Cruceru Radu <radu.cruceru@gmail.com>
2013-05-05 16:23:11 +0300

    implemented trie_match and some other tweaks

--- src/trie/trie.c
+++ src/trie/trie.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 #define WORD_CC word[word_idx]
 
@@ -25,34 +26,20 @@ void trie_insert(trie_node_t *root, char *word) {
     }
 }
 
-void trie_search(const trie_node_t *root, const char *word) {
-    int word_idx = 0;
-    int word_length = strlen(word);
-    const trie_node_t *current = root;
-
-    for (word_idx = 0; word_idx < word_length; ++word_idx) {
-        current = get_trie_node_children(current, WORD_CC);
-        if (current == NULL) {
-            return;
-        }
-    }
-    printf("found word: %s\n", word);
-    return;
-}
-
 void trie_compile(trie_node_t *root) {
     trie_node_t *current = root;
     trie_node_t *parent_fail = NULL;
-    list_node_t *kid;
+    list_node_t *kid = NULL;
     queue_t *nodes = new_queue();
     int kid_idx;
 
+    root->fail = root;
     push(nodes, (void*)root);
-    while ((current = (trie_node_t*)pop(nodes))) {
+    while ((current = (trie_node_t*)pop(nodes)) != NULL) {
         // Add all children nodes in queue
         for (kid_idx = CHILDREN_BUCKETS - 1; kid_idx >= 0; --kid_idx) {
             kid = current->children[kid_idx];
-            while (kid) {
+            while (kid != NULL) {
                 push(nodes, (void*)kid->to);
                 kid = kid->next;
             }
@@ -82,3 +69,37 @@ void trie_compile(trie_node_t *root) {
 
     return;
 }
+
+void trie_match(const trie_node_t *root, char *phrase) {
+    const trie_node_t *current = root;
+    const trie_node_t *temp;
+
+    while (*phrase) {
+
+        while((temp = get_trie_node_children(current, *phrase)) == NULL && 
+                current != root) {
+            current = current->fail;
+        }
+
+        if (0 == is_trie_node_root(current)) {
+            current = get_trie_node_children(root, *phrase);
+
+            if (current == NULL) {
+                current = root;
+            }
+
+            temp = current;
+        } else {
+            current = temp;
+        }
+    
+        while (temp != root) {
+            if (1 == is_trie_node_final(temp)) {
+                printf("found match\n");
+            }
+            temp = temp->fail;
+        }
+
+    }
+}
+
--- src/trie/trie.h
+++ src/trie/trie.h
@@ -5,7 +5,7 @@
 
 void trie_insert(trie_node_t *, const char *);
 void trie_compile(trie_node_t *);
-void trie_search(const trie_node_t *, const char *);
+void trie_match(const trie_node_t *, const char *);
 
 #endif
 
