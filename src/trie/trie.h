158ffc1ce4b402087e742003287ddd0848f9d9a6
Cruceru Radu <radu.cruceru@gmail.com>
2013-06-11 19:41:54 +0300

    forward declared parse_tree_struct and validate_tree() in trie header

--- src/trie/trie.h
+++ src/trie/trie.h
@@ -3,9 +3,12 @@
 
 #include "trie_node.h"
 
+struct parse_tree_struct;
+void validate_tree(struct parse_tree_struct *, void (*c)());
+
 void trie_insert(trie_node_t *, const char *, void *);
 void trie_compile(trie_node_t *);
-void trie_match(const trie_node_t *, const char *, void (*callback)(const trie_node_t *));
+void trie_match(const trie_node_t *, const char *, void (*c)());
 
 #endif
 
