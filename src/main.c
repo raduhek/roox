9d4526dc18f4c35200d1e2f38d289bd66836edf9
Cruceru Radu <radu.cruceru@gmail.com>
2013-06-19 23:43:18 +0300

    a lot of new shit in main.c (for test purposes)

--- src/main.c
+++ src/main.c
@@ -9,26 +9,85 @@
 #include "parse_tree/parse_tree.h"
 #include "sanitizer/sanitizer.h"
 
-#define INPUT_FILE "phrases"
-void print_found(char *id) {
-    printf("FOUND IT: %s\n", id);
+void print_found(char *text, char *id) {
+    printf("FOUND: %s -> %s", text, id);
+}
+
+void read_file(char **buffer, char *file_name) {
+    long fsize;
+    FILE *fin;
+    free(*buffer);
+    fin = fopen(file_name, "r");
+    // If file could not be opened, buffer will be null
+    if (! fin) {
+        return;
+    }
+
+    // Get file size
+    fseek(fin, 0L, SEEK_END);
+    fsize = ftell(fin);
+    rewind(fin);
+
+    *buffer = (char*) malloc(fsize * sizeof(char));
+
+    if (*buffer == NULL) {
+        printf("WARNING: file %s too big: %ld\n", file_name, fsize);
+        fclose(fin);
+        return;
+    }
+
+    if (1 != fread(*buffer, fsize, 1, fin)) {
+        printf("WARNING: could not read contents of %s\n", file_name);
+        free(*buffer);
+        fclose(fin);
+        return;
+    }
+
+    return;
+}
+
+void print_tree(parse_tree_t *t, int l) {
+int i;
+    if (t == NULL) {
+        return;
+    }
+    print_tree(t->right, l+1);
+    for (i = 0; i < l; ++i) {
+        printf("\t");
+    }
+    printf ("%c (%d)\n", t->val, t->truth_value);
+    print_tree(t->left, l+1);
 }
 
 int main(int argc, char **argv) {
     // @line holds a file entry
     char line[2048];
+    // @udid is used to identify a formula
     char udid_entry[32];
     char *udid;
+
+    // @file_buffer holds the contents of a file
+    char *file_buffer;
 
     // @phrase is the processed formula
     char *phrase;
 
     // file pointers
-    FILE *formulas_file, *texts_file;
+    FILE *formulas_file;
 
     // @ep_ok = extract_parentheses check variable
     // @bo_ok = boolean_operators check variable
     int ep_ok, bo_ok;
+
+    // @text_idx is used to read text files
+    int text_idx;
+
+    // This is used to not reset trees anymore,
+    // but know what value to take depending
+    // on what text modified the derivation tree
+    short int text_switch;
+
+    short int tree_tv;
 
     // @pt_root is the root of a syntax tree
     parse_tree_t *pt_root;
@@ -44,8 +103,8 @@ int main(int argc, char **argv) {
     // @temp_st will be used to reset syntax trees
     list_node_t *temp_st;
 
-    if (argc != 3) {
-        printf("Usage: ./%s [formulas_file] [texts_file]\n", argv[0]);
+    if (argc < 2) {
+        printf("Usage: ./%s formulas_file text_file [text_file]\n", argv[0]);
         exit(0);
     } else {
         formulas_file = freopen (argv[1], "r", stdin);
@@ -104,19 +163,40 @@ int main(int argc, char **argv) {
 
     trie_compile(trie_root);
 
-    texts_file = freopen (argv[2], "r", stdin);
-    if (texts_file == NULL) {
-        perror("Could not open texts file");
-        exit(1);
+    temp_st = syntax_trees;
+    while (temp_st) {
+        parse_tree_t *t = (parse_tree_t *) temp_st->value;
+        printf("%s\n", t->UDID);
+        print_tree(t, 0);
+        printf("\n---------------------------------------------\n");
+        temp_st = temp_st->next;
     }
 
-    while (fgets(line, sizeof(line), texts_file)) {
-        sscanf(line, "%[^\r\n]", line);
-        trie_match (trie_root, line, print_found);
+    text_switch = 0;
 
+    for (text_idx = 2; text_idx < argc; ++text_idx) {
+        text_switch = 1 - text_switch;
+
+        read_file(&file_buffer, argv[text_idx]);
+        // warnings are printed in read_file
+        if (NULL == file_buffer) continue;
+
+        // Match formulas
+        trie_match (trie_root, file_buffer, text_switch, validate_tree);
+        
+        // Check syntax trees for root truth values
         temp_st = syntax_trees;
         while (temp_st) {
-            reset_parse_tree((parse_tree_t*) temp_st->value);
+            parse_tree_t *t = (parse_tree_t*) temp_st->value;
+
+            tree_tv = operator_truth_value(t, text_switch, 1);
+            
+            t->truth_value = t->initial_truth_value;
+
+            if (tree_tv) {
+                print_found(argv[text_idx], t->UDID);
+            }
+
             temp_st = temp_st->next;
         }
     }
